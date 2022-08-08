2ec73d0ff43c94d1a0996d176146f7728719b73d
thanhneymarjr <103953058+thanhneymarjr@users.noreply.github.com>
2022-08-08 17:03:26 +0700

    Add files via upload

---
+++ build/precache-manifest.aa9f938d348cb5dd3a113d0153fc7f64.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771a1ce37fa66c869090a71353ab8bc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c03b5f744b6d576c3ba",
+    "url": "/static/css/2.e4777ce8.chunk.css"
+  },
+  {
+    "revision": "a63205df5b516687b5ba",
+    "url": "/static/css/main.f75ea307.chunk.css"
+  },
+  {
+    "revision": "8c03b5f744b6d576c3ba",
+    "url": "/static/js/2.b11e53e5.chunk.js"
+  },
+  {
+    "revision": "a810d783338ef9ba2bd2770ee4bc75fd",
+    "url": "/static/js/2.b11e53e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a63205df5b516687b5ba",
+    "url": "/static/js/main.f55e3731.chunk.js"
+  },
+  {
+    "revision": "074d9be63bd44731693d",
+    "url": "/static/js/runtime-main.e763871d.js"
+  },
+  {
+    "revision": "f2425f17a305204ed707c1a4c455bd0e",
+    "url": "/static/media/call-to-action-paralax.f2425f17.jpg"
+  },
+  {
+    "revision": "35bed3b48acfa24008b0aa7b8b8c1050",
+    "url": "/static/media/cover-photo20.35bed3b4.jpg"
+  },
+  {
+    "revision": "c53d04ad4712f444f844713ca0be75a6",
+    "url": "/static/media/fb-subheader-2.c53d04ad.jpg"
+  },
+  {
+    "revision": "ac460e89f00b7d194d13c2844b01d8b0",
+    "url": "/static/media/fb-subheader-4.ac460e89.jpg"
+  },
+  {
+    "revision": "162ff4008c83359a06f3809c90223b52",
+    "url": "/static/media/home-top-1.162ff400.jpg"
+  },
+  {
+    "revision": "dab6f3cd7b3079a88ce8097368831415",
+    "url": "/static/media/how-to-work1.dab6f3cd.png"
+  },
+  {
+    "revision": "815ace79df7a4cc42f6ce3e2d4c4f016",
+    "url": "/static/media/how-to-work2.815ace79.png"
+  },
+  {
+    "revision": "02d846b8ff5c3ef15bce4f3f80b6b75e",
+    "url": "/static/media/how-to-work3.02d846b8.png"
+  },
+  {
+    "revision": "22be2bce134558546fd1288668ad01c6",
+    "url": "/static/media/listing-logo03.22be2bce.png"
+  },
+  {
+    "revision": "9debe726febc8dcaca36f38396081398",
+    "url": "/static/media/listing-logo09.9debe726.png"
+  },
+  {
+    "revision": "c9102623a160b0b860ebdaccb5846dec",
+    "url": "/static/media/listing-logo12.c9102623.png"
+  },
+  {
+    "revision": "1dd39ed1d99822bf21122e8ce6012a53",
+    "url": "/static/media/listing-logo13.1dd39ed1.png"
+  }
+]);
